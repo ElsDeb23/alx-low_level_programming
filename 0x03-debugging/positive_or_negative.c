914e4f9c621a24de47bbd9abde3cecdf243f3b2e
ElsDeb23 <akushikadebrah@gmail.com>
2023-06-22 18:48:26 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: 'the program will check if a number is positive or negative'
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+{
