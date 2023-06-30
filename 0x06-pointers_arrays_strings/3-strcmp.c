1b6f2e5ccae3d78cdfebbf267a1ef581b91307b0
ElsDeb23 <akushikadebrah@gmail.com>
2023-06-29 17:31:28 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- * Return: s1[i] - s2[i]
+ * _strcmp - A function that compares two string values
+ * @s1: an input value
+ * @s2: an input value
+ *
+ * Return: s1[m] - s2[m]
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int m;
 
-	i = 0;
-	while (s1[i] != '0\' && s2[i] != '\0')
+	m = 0;
+	while (s1[m] != '\0' && s2[m] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[m] != s2[m])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[m] - s2[m]);
 		}
-		i++;
+		m++;
 	}
 	return (0);
 }
+
